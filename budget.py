00ddd2ab564de4d9c6019b10a09f8c3ea4a58e19
adamstok <adamstokowski@outlook.com>
2020-12-21 09:35:06 +0100

    deposit

--- budget.py
+++ budget.py
@@ -1,5 +1,13 @@
 class Category:
-    pass
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description=""):
+        pass
 
 
 def create_spend_chart(categories):
